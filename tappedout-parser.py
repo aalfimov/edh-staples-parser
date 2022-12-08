3a23bb6af23f824e98ba6518e8f534bb73244778
Александр Алфимов <39705314+aalfimov@users.noreply.github.com>
2022-12-09 00:07:26 +0200

    aalfimov
    add time count

--- tappedout-parser.py
+++ tappedout-parser.py
@@ -1,15 +1,7 @@
-import requests
 from html.parser import HTMLParser
 
-
-# Competitive edh staple statistics
-
-list_of_decks = []
-parsed_staple_dictionary = {}
-url_to_search_deck = f'https://tappedout.net/mtg-decks/search/?q=&format=edh&cards=mana-crypt&hubs=competitive' \
-                     f'&price_min=&price_max=&o=-date_updated&submit=Filter+results&p={i + 1}&page={i + 1} '
-pages_to_grabe = 1
-file_to_save = 'parsed_data_codie.txt'
+import requests
+import time
 
 
 class MyDeckHTMLParser(HTMLParser):
@@ -49,7 +41,7 @@ def tappedout_deck_parser(url_to_parse):
         index_start = index_finish
 
 
-def tappedout_cedh_deck_searcher(url_searcher) -> object:
+def tappedout_cedh_deck_searcher(url_searcher):
     index_start = 0
     list_of_deck_req = requests.get(url_searcher)
     html = str(list_of_deck_req.content)
@@ -62,26 +54,33 @@ def tappedout_cedh_deck_searcher(url_searcher) -> object:
 
 
 # main program
-print("Собираю ссылки...")
+if __name__ == '__main__':
+    # Competitive edh staple statistics
+    start = time.perf_counter()
+    list_of_decks = []
+    parsed_staple_dictionary = {}
+    pages_to_grabe = 1
+    file_to_save = 'parsed_data_codie.txt'
+    print("Catch urls...")
+    for i in range(pages_to_grabe):
+        url_to_search_deck = f'https://tappedout.net/mtg-decks/search/?q=&format=edh&cards=mana-crypt&hubs=competitive' \
+                             f'&price_min=&price_max=&o=-date_updated&submit=Filter+results&p={i + 1}&page={i + 1} '
+        tappedout_cedh_deck_searcher(url_to_search_deck)
+    print("Found ", len(list_of_decks), " decks")
+    for link_index in range(len(list_of_decks)):
+        tappedout_deck_parser(list_of_decks[link_index])
+        print(link_index + 1, "deck done")
+    print("Sort the result")
+    sorted_tuples = sorted(parsed_staple_dictionary.items(), key=lambda item: item[1])
+    parsed_staple_dictionary = {k: v for k, v in sorted_tuples[::-1]}
+    print(f"Save the result in {file_to_save}")
 
-for i in range(pages_to_grabe):
-    tappedout_cedh_deck_searcher(url_to_search_deck)
+    f = open(file_to_save, 'a')
+    for card in parsed_staple_dictionary:
+        f.writelines(f"{parsed_staple_dictionary[card]} {card}\n")
+    f.writelines(f"{list_of_decks}\n")
+    f.close()
+    finish = time.perf_counter()
 
-print("Нашел", len(list_of_decks), "колод")
-for link_index in range(len(list_of_decks)):
-    tappedout_deck_parser(list_of_decks[link_index])
-    print(link_index + 1, "deck done")
-
-print("Сортирую результат")
-sorted_tuples = sorted(parsed_staple_dictionary.items(), key=lambda item: item[1])
-parsed_staple_dictionary = {k: v for k, v in sorted_tuples[::-1]}
-
-print(f"Сохраняю результат в {file_to_save}")
-f = open(file_to_save, 'a')
-for card in parsed_staple_dictionary:
-    f.writelines(f"{parsed_staple_dictionary[card]} {card}\n")
-
-f.writelines(f"{list_of_decks}\n")
-f.close()
-print("Готово")
-print(f'parsed_staple_dictionary - {parsed_staple_dictionary}')
+    print(f'parsed_staple_dictionary - {parsed_staple_dictionary}')
+    print(f"Finish in {round(finish - start, 2)} seconds")
